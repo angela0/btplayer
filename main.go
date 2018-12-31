c537144e0f8263ae9c993347e6fe59f6f55526d5
angela <zuchuang1@gmail.com>
2018-12-31 15:25:19 +0800

    ok

--- main.go
+++ main.go
@@ -7,7 +7,6 @@ import (
 	"encoding/hex"
 	"encoding/json"
 	"fmt"
-	"io"
 	"log"
 	"net/http"
 	"os"
@@ -22,6 +21,13 @@ import (
 	"gopkg.in/mgo.v2/bson"
 )
 
+type Config struct {
+	DBUrl         string
+	DBName        string
+	CookieExpires time.Duration
+	DownloadDir   string
+}
+
 type Torrent struct {
 	T       *torrent.Torrent
 	RefCnt  int
@@ -29,14 +35,15 @@ type Torrent struct {
 }
 
 var (
-	session  *mgo.Session
-	userColl *mgo.Collection
+	session     *mgo.Session
+	userColl    *mgo.Collection
+	loginColl   *mgo.Collection
+	torrentColl *mgo.Collection
+
+	config Config
 
 	tClient *torrent.Client
-	file    io.ReadSeeker
 	Map     sync.Map
-
-	downloadDir string = "download"
 )
 
 func checkLogin(req *http.Request) bool {
@@ -44,7 +51,11 @@ func checkLogin(req *http.Request) bool {
 	if err != nil {
 		return false
 	}
-	if err := userColl.Find(bson.M{"id": id.Value}).One(nil); err != nil {
+	name, err := req.Cookie("name")
+	if err != nil {
+		return false
+	}
+	if err := loginColl.Find(bson.M{"id": id.Value, "username": name.Value}).One(nil); err != nil {
 		if err != mgo.ErrNotFound {
 			log.Println(err)
 		}
@@ -63,7 +74,7 @@ func getTorrent(hash []byte) (*torrent.Torrent, bool) {
 	return tClient.Torrent(h)
 }
 
-func HandleAddMagnet(w http.ResponseWriter, req *http.Request) {
+func handleAddMagnet(w http.ResponseWriter, req *http.Request) {
 	statusCode, msg := 200, "add failed"
 
 	defer func() {
@@ -77,14 +88,98 @@ func HandleAddMagnet(w http.ResponseWriter, req *http.Request) {
 		return
 	}
 	infohash := req.URL.Query().Get("hash")
-	if _, err := tClient.AddMagnet(fmt.Sprintf("magnet:?xt=urn:btih:%s", infohash)); err != nil {
+	t, err := tClient.AddMagnet(fmt.Sprintf("magnet:?xt=urn:btih:%s", infohash))
+	if err != nil {
 		log.Println(err)
 		statusCode = 500
 		return
 	}
+	if _, err := torrentColl.Upsert(bson.M{"_id": infohash}, bson.M{"_id": infohash}); err != nil {
+		t.Drop()
+		log.Println(err)
+		statusCode = 500
+		return
+	}
+	username, _ := req.Cookie("name")
+	if err := userColl.Update(bson.M{"username": username.Value, "hashes": bson.M{"$nin": []string{infohash}}}, bson.M{"$push": bson.M{"hashes": infohash}}); err != nil {
+		if err != mgo.ErrNotFound {
+			log.Println(err)
+			statusCode = 500
+			return
+		}
+	}
 }
 
-func HandleFile(w http.ResponseWriter, req *http.Request) {
+func handleDeleteMagnet(w http.ResponseWriter, req *http.Request) {
+	statusCode, msg := 200, "delete failed"
+
+	defer func() {
+		w.Header().Set("X-Message", msg)
+		w.WriteHeader(statusCode)
+	}()
+
+	if !checkLogin(req) {
+		statusCode = 403
+		msg = "not login"
+		return
+	}
+	infohash := req.URL.Query().Get("hash")
+	username, _ := req.Cookie("name")
+	if err := userColl.Update(bson.M{"username": username.Value, "hashes": infohash}, bson.M{"$pull": bson.M{"hashes": infohash}}); err != nil {
+		if err == mgo.ErrNotFound {
+			statusCode, msg = 400, "fuck infohash"
+			return
+		}
+		log.Println(err)
+		statusCode, msg = 500, "sorry, you can't do that now, please retry"
+		return
+	}
+}
+
+func handleInfos(w http.ResponseWriter, req *http.Request) {
+	var (
+		ret struct {
+			Hashes    []string `json:"hashes"`
+			PageTotal int      `json:"pageTotal"`
+		}
+		jsonRet    []byte
+		err        error
+		statusCode = 200
+		msg        = "bad request"
+	)
+
+	defer func() {
+		if statusCode != 200 {
+			w.Header().Set("X-Message", msg)
+		}
+		w.WriteHeader(statusCode)
+		w.Write(jsonRet)
+	}()
+
+	if !checkLogin(req) {
+		statusCode, msg = 403, "not login"
+		return
+	}
+
+	switch infoTypes := req.URL.Query().Get("type"); infoTypes {
+	case "all":
+	default:
+		username, _ := req.Cookie("name")
+		if err = userColl.Find(bson.M{"username": username.Value}).One(&ret); err != nil {
+			log.Println(err)
+			statusCode, msg = 500, "something goes wrong, please contacts administator"
+			return
+		}
+		ret.PageTotal = len(ret.Hashes)
+	}
+	if jsonRet, err = json.Marshal(ret); err != nil {
+		log.Println(err)
+		statusCode, msg = 500, "something goes wrong, please contacts administator"
+		return
+	}
+}
+
+func handleFile(w http.ResponseWriter, req *http.Request) {
 	statusCode, msg := 200, "bad request"
 
 	defer func() {
@@ -133,61 +228,30 @@ func newWebSocket(w http.ResponseWriter, req *http.Request, cros bool) (*websock
 	return upgrader.Upgrade(w, req, nil)
 }
 
-func handlePlayerClose(w http.ResponseWriter, req *http.Request) {
-	c, err := newWebSocket(w, req, true)
-	if err != nil {
-		log.Println(err)
-		return
-	}
-	defer c.Close()
-
-	ch := make(chan struct{})
-	c.SetCloseHandler(func(code int, text string) error {
-		ch <- struct{}{}
-		return nil
-	})
-	<-ch
-}
-
-func HandleWs(w http.ResponseWriter, req *http.Request) {
-	// if !checkLogin(req) {
-	// 	w.Header().Set("X-Message", "not login")
-	// 	w.WriteHeader(403)
-	// 	return
-	// }
-	c, err := newWebSocket(w, req, true)
-	if err != nil {
-		log.Println(err)
-		return
-	}
-	defer c.Close()
-
-	var info struct {
-		InfoHash string   `json:"infohash"`
-		Name     string   `json:"name"`
-		Size     int64    `json:"size"`
-		Files    []string `json:"files"`
-	}
-
-	_, msg, err := c.ReadMessage()
-	if err != nil {
-		log.Println(err)
-		return
-	}
-
-	t, ok := getTorrent(msg)
+func processWsInfo(c *websocket.Conn, data string) {
+	t, ok := getTorrent([]byte(data))
 	if !ok {
 		log.Println("maybe have no this infohash")
 		return
 	}
 
+	var info struct {
+		Type string `json:"type"`
+		Data struct {
+			InfoHash string   `json:"infohash"`
+			Name     string   `json:"name"`
+			Size     int64    `json:"size"`
+			Files    []string `json:"files"`
+		} `json:"data"`
+	}
 	select {
 	case <-t.GotInfo():
-		info.Name = t.Name()
-		info.InfoHash = t.InfoHash().String()
-		info.Size = t.Info().TotalLength()
+		info.Type = "info"
+		info.Data.Name = t.Name()
+		info.Data.InfoHash = t.InfoHash().String()
+		info.Data.Size = t.Info().TotalLength()
 		for _, f := range t.Files() {
-			info.Files = append(info.Files, f.Path())
+			info.Data.Files = append(info.Data.Files, f.Path())
 		}
 	}
 	if err := c.WriteJSON(info); err != nil {
@@ -195,8 +259,47 @@ func HandleWs(w http.ResponseWriter, req *http.Request) {
 		return
 	}
 }
+func processWsPlayer(c *websocket.Conn, data string) {
 
-func HandleSearch(w http.ResponseWriter, req *http.Request) {
+}
+func handleWs(w http.ResponseWriter, req *http.Request) {
+	if !checkLogin(req) {
+		w.Header().Set("X-Message", "not login")
+		w.WriteHeader(403)
+		return
+	}
+	c, err := newWebSocket(w, req, true)
+	if err != nil {
+		log.Println(err)
+		return
+	}
+	defer c.Close()
+
+	for {
+		var jsonData struct {
+			Type string
+			Data string
+		}
+		if err := c.ReadJSON(&jsonData); err != nil {
+			log.Println(err)
+			if websocket.IsCloseError(err, websocket.CloseGoingAway, websocket.CloseAbnormalClosure) {
+				log.Println(c.Close())
+				break
+			}
+			c.WriteMessage(0, []byte(`{"type": "error", "data": "sorry for that"}`))
+			continue
+		}
+		if jsonData.Type == "info" {
+			go processWsInfo(c, jsonData.Data)
+		} else if jsonData.Type == "player" {
+			go processWsPlayer(c, jsonData.Data)
+		} else {
+			c.WriteMessage(0, []byte(`{"type": "error", "data": "fuck you"}`))
+		}
+	}
+}
+
+func handleSearch(w http.ResponseWriter, req *http.Request) {
 	var (
 		msg        = "ok"
 		statusCode = 200
@@ -273,22 +376,27 @@ func handleLogin(w http.ResponseWriter, req *http.Request) {
 			return
 		}
 		if req.Method == "POST" {
-			setCookie("id", login.Id, "/", time.Now().Add(24*365*time.Hour))
-			setCookie("name", login.Username, "/", time.Now().Add(24*365*time.Hour))
+			setCookie(w, "id", login.Id, "/", time.Now().Add(24*365*time.Hour))
+			setCookie(w, "name", login.Username, "/", time.Now().Add(24*365*time.Hour))
 		}
 	}()
 
 	//// check if login before
 	if req.Method == "GET" {
-		cookie, err := req.Cookie("id")
+		id, err := req.Cookie("id")
 		if err != nil {
-			statusCode, msg = 403, "login expired"
+			statusCode, msg = 403, "not login"
 			return
 		}
-		if err := userColl.Find(bson.M{"id": cookie.Value}).One(nil); err == mgo.ErrNotFound {
-			statusCode, msg = 403, "login expired"
+		if err := loginColl.Update(bson.M{"id": id.Value}, bson.M{"$set": bson.M{"createAt": time.Now()}}); err != nil {
+			if err == mgo.ErrNotFound {
+				statusCode, msg = 403, "login expired"
+				return
+			}
+			statusCode, msg = 500, "login status not avaiable"
 			return
 		}
+		statusCode, msg = 200, "login status is ok"
 		return
 	}
 
@@ -308,7 +416,7 @@ func handleLogin(w http.ResponseWriter, req *http.Request) {
 	}
 
 	login.Id = fmt.Sprintf("%x", sha1.Sum([]byte(login.Username+login.Password+time.Now().String())))
-	if err := userColl.Update(bson.M{"username": login.Username}, bson.M{"$set": bson.M{"id": login.Id, "last": time.Now().Unix(), "ip": req.Header.Get("X-Real-IP")}}); err != nil {
+	if _, err := loginColl.Upsert(bson.M{"username": login.Username}, bson.M{"username": login.Username, "id": login.Id, "createAt": time.Now(), "ip": req.Header.Get("X-Real-IP")}); err != nil {
 		log.Println(err)
 		statusCode, msg = 500, "Something goes wrong, please contracts administator"
 		return
@@ -316,61 +424,78 @@ func handleLogin(w http.ResponseWriter, req *http.Request) {
 }
 
 func handleLogout(w http.ResponseWriter, req *http.Request) {
-	statusCode := 403
-	msg := "sb"
+	var (
+		id, name   *http.Cookie
+		msg        = "sb"
+		statusCode = 403
+		err        error
+	)
 	defer func() {
 		w.Header().Set("X-Message", msg)
 		w.WriteHeader(statusCode)
 	}()
 
-	id, err := req.Cookie("id")
-	if err != nil {
+	if id, err = req.Cookie("id"); err != nil {
+		log.Println(err)
 		return
 	}
-	name, err := req.Cookie("name")
-	if err != nil {
+	if name, err = req.Cookie("name"); err != nil {
+		log.Println(err)
 		return
 	}
-	if err := userColl.Update(bson.M{"username": name.Value, "id": id.Value}, bson.M{"$set": bson.M{"id": ""}}); err != nil {
+	if err := loginColl.Remove(bson.M{"username": name.Value, "id": id.Value}); err != nil {
 		if err != mgo.ErrNotFound {
+			log.Println(err)
 			msg = "something goes wrong, please retry"
 			statusCode = 500
 		}
 		return
 	}
-	statusCode = 200
-	msg = "ok"
+	statusCode, msg = 200, "ok"
 }
 
 func main() {
 	var err error
 	log.SetFlags(log.Lshortfile)
 
+	// init config
+	config.DBUrl = "127.0.0.1"
+	config.DBName = "btplayer"
+	config.CookieExpires = time.Hour * 24 * 30
+	config.DownloadDir = "download"
+	// end init config
+
 	search.Init()
 
-	session, err = mgo.Dial("127.0.0.1")
-	if err != nil {
+	if session, err = mgo.Dial(config.DBUrl); err != nil {
 		log.Fatal(err)
 	}
-	userColl = session.DB("btplayer").C("user")
-
-	if err := os.RemoveAll(downloadDir); err != nil && err != os.ErrNotExist {
+	db := session.DB(config.DBName)
+	userColl = db.C("user")
+	loginColl = db.C("login")
+	torrentColl = db.C("torrent")
+	if err := loginColl.EnsureIndex(mgo.Index{Key: []string{"createdAt"}, ExpireAfter: config.CookieExpires}); err != nil {
 		log.Fatal(err)
 	}
-	if err := os.Mkdir(downloadDir, 0666); err != nil && err != os.ErrExist {
+	torrentColl.DropCollection()
+	if err := os.RemoveAll(config.DownloadDir); err != nil && err != os.ErrNotExist {
 		log.Fatal(err)
 	}
-	config := torrent.NewDefaultClientConfig()
-	config.DataDir = downloadDir
-	if tClient, err = torrent.NewClient(config); err != nil {
+	if err := os.Mkdir(config.DownloadDir, 0660); err != nil && err != os.ErrExist {
+		log.Fatal(err)
+	}
+	tConfig := torrent.NewDefaultClientConfig()
+	tConfig.DataDir = config.DownloadDir
+	if tClient, err = torrent.NewClient(tConfig); err != nil {
 		log.Fatal(err)
 	}
 
 	http.HandleFunc("/login", handleLogin)
 	http.HandleFunc("/logout", handleLogout)
-	http.HandleFunc("/info", HandleAddMagnet)
-	http.HandleFunc("/search", HandleSearch)
-	http.HandleFunc("/file", HandleFile)
-	http.HandleFunc("/ws", HandleWs)
+	http.HandleFunc("/info", handleAddMagnet)
+	http.HandleFunc("/infos", handleInfos)
+	http.HandleFunc("/search", handleSearch)
+	http.HandleFunc("/file", handleFile)
+	http.HandleFunc("/ws", handleWs)
 	http.ListenAndServe("127.0.0.1:8084", nil)
 }
